95510b2d015c631768ddc91f105b7e854935a922
Michael Lucy <mlucy@rethinkdb.com>
2015-05-13 18:13:47 -0700

    CAS passing!

--- rdb/src/jepsen/system/rdb.clj
+++ rdb/src/jepsen/system/rdb.clj
@@ -85,37 +85,79 @@
 (defn db []
   (reify db/DB
     (setup! [this test node]
+      (debian/install [:libprotobuf7 :libicu48 :psmisc])
       (let [file "rethinkdb/build/debug/rethinkdb"]
         (info node (str "Copying ~/" file "..."))
         (copy-from-home file)
         (info node (str "Copying ~/" file " DONE!")))
-      (info node (c/ssh* {:cmd "hostname"})))
+      (info node "Starting...")
+      (c/ssh* {:cmd "
+rm -rf rethinkdb_data
+chmod a+x rethinkdb
+nohup ./rethinkdb --bind all \\
+                  -j n1:29015 -j n2:29015 -j n3:29015 -j n4:29015 -j n5:29015 \\
+                  >1.out 2>2.out &
+sleep 5
+"})
+      (info node "Starting DONE!")
+      (if (= node :n1)
+        (do
+          (info node "Creating table...")
+          (info node (str `(connect :host ~(name node) :port 28015)))
+          (with-open [conn (connect :host (name node) :port 28015)]
+            (r/run (r/db-create "jepsen") conn)
+            (r/run (r/table-create (r/db "jepsen") "cas") conn)
+            (r/run (r/insert (r/table (r/db "jepsen") "cas") {:id 0 :val nil}) conn))
+          (info node "Creating table DONE!"))
+        (info node "Not creating table.")))
     (teardown! [_ test node]
       (info node "Tearing down db...")
+      (c/ssh* {:cmd "
+killall rethinkdb
+f=0
+while [[ \"`ps auxww | grep rethinkdb | grep -v grep`\" ]] && [[ \"$f\" -lt 10 ]]; do
+  sleep 1
+  f=$((f+1))
+done
+if [[ \"$f\" -ge 10 ]]; then
+  killall -9 rethinkdb
+fi
+"})
       (info node "Tearing down db DONE!"))))
-
-(def glob (atom nil))
 
 (defrecord CASClient [k client]
   client/Client
   (setup! [this test node]
     (info node "Connecting CAS Client...")
-    (info node "Connecting CAS Client DONE!")
-    (assoc this :client client))
+    (info node (str `(connect :host ~(name node) :port 28015)))
+    (let [conn (connect :host (name node) :port 28015)]
+      (info node "Connecting CAS Client DONE!")
+      (assoc this :conn conn :node node)))
   ;;    [client (v/connect (str "http://" (name node) ":2379"))]
   ;;      (v/reset! client k (json/generate-string nil))
   ;;      (assoc this :client client)))
+
   (invoke! [this test op]
-    (let [fail (if (= :read (:f op)) :fail :info)]
+    (let [fail (if (= :read (:f op)) :fail :info)
+          row (r/get (r/table (r/db "jepsen") "cas") 0)]
       (try+
        (case (:f op)
-         :read (assoc op :type :ok :value @glob)
-         :write (do (swap! glob (fn [_] (:value op)))
+         :read (assoc op :type :ok :value (r/run (r/get-field row "val") (:conn this)))
+         :write (do (r/run
+                      (r/update row {:val (:value op)})
+                      (:conn this))
                     (assoc op :type :ok))
-         :cas   (let [[value value'] (:value op)
-                      ok? (if (= @glob value)
-                            (swap! glob (fn [_] value')))]
-                  (assoc op :type (if ok? :ok :fail))))
+         :cas (let [[value value'] (:value op)
+                    res (r/run
+                          (r/update
+                           row
+                           (r/fn [row]
+                             (r/branch
+                              (r/eq (r/get-field row "val") value)
+                              {:val value'}
+                              (r/error "abort"))))
+                          (:conn this))]
+                (assoc op :type (if (= (:errors res) 0) :ok :fail))))
        (catch java.net.SocketTimeoutException e
          (assoc op :type fail :value :timed-out))
        (catch (and (instance? clojure.lang.ExceptionInfo %)) e
